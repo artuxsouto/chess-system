9c41e2dc226d563f10799a334086708d05b31466
Artux Notebook <artursiqueirasouto@hotmail.com>
2022-04-12 00:06:38 -0300

    Programa criado

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,8 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println();
+		System.out.println("Hello World!!");
 	}
 
 }
